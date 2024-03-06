035b870824de3d9caf4d07b962313538d6738b67
alexeus8776 <alekseevaleksey124@gmail.com>
2024-03-06 19:36:31 +0900

    header obratno polozhil

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,6 +7,25 @@
     @yield('head')
 </head>
 <body>
+    <header>
+            <div class='bg_header'></div>
+            <div class='header_info'>
+                <a href=""><img src="{{asset('img/лого.png')}}" alt="Логотип" class='logo'></a>
+                <div class='header_a'>
+                    <a href="#">Список финалистов</a>
+                    <a href="#">О форуме</a>
+                    <a href="#">Положение</a>
+                    <a href="#">Программа форума</a>
+                    <a href="#">Организаторы</a>
+                    <a href="#">Сертификаты</a>
+                    <a href="#">Координаторы</a>
+                    <a href="#">Сборник</a>
+                </div>
+                <div class='profile'>
+                    <a href="#"><img src="{{asset('img/reg.png')}}" alt=""></a>
+                </div>
+            </div>
+        </header>
     @yield('content')
     <footer>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,25 +10,6 @@
 
 @section('content')
     <section class='bg'>
-        <header>
-            <div class='bg_header'></div>
-            <div class='header_info'>
-                <a href=""><img src="{{asset('img/лого.png')}}" alt="Логотип" class='logo'></a>
-                <div class='header_a'>
-                    <a href="#">Список финалистов</a>
-                    <a href="#">О форуме</a>
-                    <a href="#">Положение</a>
-                    <a href="#">Программа форума</a>
-                    <a href="#">Организаторы</a>
-                    <a href="#">Сертификаты</a>
-                    <a href="#">Координаторы</a>
-                    <a href="#">Сборник</a>
-                </div>
-                <div class='profile'>
-                    <a href="#"><img src="{{asset('img/reg.png')}}" alt=""></a>
-                </div>
-            </div>
-        </header>
         <main>
             <div class='bg_main'>
                 <div class='bg_main_info'>
