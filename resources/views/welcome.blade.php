c8a87b94656158ea115fb98c469d6b7ab286828f
alexeus8776 <alekseevaleksey124@gmail.com>
2024-03-11 13:08:24 +0900

    добавил новые разделы

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,6 +50,103 @@
                                     <hr>
                                 </div>
                         </div>
+                        <div class='competition'>
+                            <div class='competition_bg' style='background-image: url("../img/"); background-size: cover;'></div>
+                                <div class='competition_info'>
+                                    <p>Выставка инженерных проектов #Автобот.2023</p>
+                                    <hr>
+                                </div>
+                        </div>
+                        <div class='competition'>
+                            <div class='competition_bg' style='background-image: url("../img/"); background-size: cover;'></div>
+                                <div class='competition_info'>
+                                    <p>Выставка инженерных проектов #Автобот.2023</p>
+                                    <hr>
+                                </div>
+                        </div>
+                        <div class='competition'>
+                            <div class='competition_bg' style='background-image: url("../img/"); background-size: cover;'></div>
+                                <div class='competition_info'>
+                                    <p>Выставка инженерных проектов #Автобот.2023</p>
+                                    <hr>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class='orgs_main'>
+                <h1>ОРГАНИЗАТОРЫ</h1>
+                <div class='orgs'>
+                    <div class='org'>
+                        <img src="{{asset('img/org1.png')}}" alt="Организации">
+                    </div>
+                    <div class='org'>
+                        <img src="{{asset('img/org2.png')}}" alt="Организации">
+                    </div>
+                    <div class='org'>
+                        <img src="{{asset('img/org3.png')}}" alt="Организации">
+                    </div>
+                    <div class='org'>
+                        <img src="{{asset('img/org4.png')}}" alt="Организации">
+                    </div>
+                    <div class='org'>
+                        <img src="{{asset('img/org5.png')}}" alt="Организации">
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class='partns_main'>
+                <h1>ПАРТНЕРЫ</h1>
+                <div class='partns'>
+                    <div class='partn'>
+                        <img src="{{asset('img/partn1.png')}}" alt="Партнеры">
+                    </div>
+                    <div class='partn'>
+                        <img src="{{asset('img/partn2.png')}}" alt="Партнеры">
+                    </div>
+                    <div class='partn'>
+                        <img src="{{asset('img/partn3.png')}}" alt="Партнеры">
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class='leaders_main'>
+                <h1>ОБЩЕЕ РУКОВОДСТВО</h1>
+                <div class='leaders'>
+                    <div class='leader'>
+                        <img src="{{asset('img/leader1.png')}}" alt="">
+                        <div class='leader_info'>
+                            <p>имя</p>
+                            <p>имя</p>
+                        </div>
+                    </div>
+                    <div class='leader'>
+                        <img src="{{asset('img/leader1.png')}}" alt="">
+                        <div class='leader_info'>
+                            <p>имя</p>
+                            <p>имя</p>
+                        </div>
+                    </div>
+                    <div class='leader'>
+                        <img src="{{asset('img/leader1.png')}}" alt="">
+                        <div class='leader_info'>
+                            <p>имя</p>
+                            <p>имя</p>
+                        </div>
+                    </div>
+                    <div class='leader'>
+                        <img src="{{asset('img/leader1.png')}}" alt="">
+                        <div class='leader_info'>
+                            <p>имя</p>
+                            <p>имя</p>
+                        </div>
+                    </div>
+                    <div class='leader'>
+                        <img src="{{asset('img/leader1.png')}}" alt="">
+                        <div class='leader_info'>
+                            <p>имя</p>
+                            <p>имя</p>
+                        </div>
                     </div>
                 </div>
             </div>
