fdf5387f9e7b3031715d974c2448d92c0fc7036e
Your Name <you@example.com>
2024-03-11 16:07:18 +0900

    добавил простенькую auth

--- resources/views/auto.blade.php
+++ resources/views/auto.blade.php
@@ -6,14 +6,25 @@
 
 
 @section('content')
-
-<h1>Вход</h1>
-<div>
-    <label for="login">Логин</label>
-    <input type="text" name='login'>
-</div>
-<div>
-    <label for="pass">Логин</label>
-    <input type="password" name='pass'>
-</div>
+<main>
+    <section class='auto_section'>
+        <div class='auto'>
+            <div>
+                <h1>Вход</h1>
+            </div>
+            <table class='auto_table'>
+                <form action="" method="post">
+                    <tr>
+                        <td><label for="login">Логин</label></td>
+                        <td><input type="text" name='login'></td>
+                    </tr>
+                    <tr>
+                        <td><label for="pass">Пароль</label></td>
+                        <td><input type="password" name='pass'></td>
+                    </tr>
+                </form>
+            </table>
+        </div>
+    </section>
+</main>
 @endsection
