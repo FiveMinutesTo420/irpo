ac1b8aee9ba8c4bd3884c4767a79829c1eb17950
Aiaal <aiaal8964@mail.ru>
2024-04-01 17:57:54 +0900

    Добавление баннера, добавление экспертов в разработке

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Event;
+use App\Models\Organizer;
+
 class AdminController extends Controller
 {
     public function __invoke(Request $request){
@@ -15,6 +17,19 @@ class AdminController extends Controller
     //Функция создания мероприятия
     public function createEvent(Request $request){
         //Создание мероприятия
+        
+
+        //Докончить добавление множества экспертов. 
+        foreach($request->name as $name){
+            //Вывод всех введеных экспертов.
+            $expert = new Organizer();
+            $expert->name = $name;
+            $expert->surname = $request->surname[array_search($name,$request->name)];
+            $expert->patronymic = $request->patronymic[array_search($name,$request->name)];
+            $expert->photo = $request->photo[array_search($name,$request->name)];
+        }
+        dd(1);
+        
         $event = new Event();
         $event->title = $request->input('title');
         if($request->exists('main')){
@@ -31,7 +46,9 @@ class AdminController extends Controller
     
         //Сохранение в базу и возвращение
         $event->save();
+
         return redirect()->back()->with('message','Мероприятие успешно создано');
+
     }
     public function deleteEvent(Request $request){
         $data = $request->validate([
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -19,12 +19,24 @@
                 <input type="checkbox" name="main">
                 Основное мероприятие
             </div>
+            <div class="experts">
+
+            </div>
+
+            <input type="button" value="Добавить эксперта" id="addExpert">
+
+
+
+
+
+
+
+
 
             <input type="submit" value="Создать">
         </form>
         <div class="events-list">
             <h4>Список мероприятий</h4>
-            
             @foreach($events as $event)
             <div class="event">
                 <a href="{{route('event',$event->slug)}}">{{$event->title}} | {{$event->created_at}}</a>
@@ -39,13 +51,18 @@
                         <input type="hidden" name="id" value="{{$event->id}}">
                         <input type="submit" value="Редактировать" style="background: green">
                     </form>
- 
                 </div>
-
             </div>
             @endforeach
         </div>
     </section>
-
 </main>
+<script src="js/jquery.js"></script>
+<script>
+let count = 0
+$('#addExpert').click(function(){
+    count += 1
+    $('.experts').append('<div class="expert-fields"> <p>Эксперт №' + count +'</p><input type="text" name="name[]" placeholder="Имя"><input type="text" name="surname[]"  placeholder="Фамилия"><input type="text" name="patronymic[]"  placeholder="Отчество"><textarea name="description[]" id="" cols="30" rows="6"  placeholder="Добавьте описание"></textarea>    Фотография <input type="file" name="expert-image[]"></div>')
+})
+</script>
 @endsection
